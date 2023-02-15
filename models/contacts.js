2df347acfd7cbc77210b889875e08f09864bd712
efirnet <efirnet@gmail.com>
2023-02-15 23:24:14 +0200

    fix

--- models/contacts.js
+++ models/contacts.js
@@ -61,22 +61,3 @@ module.exports = {
   removeContact,
   updateContact,
 };
-
-
-// const listContacts = async () => { }
-
-// const getContactById = async (contactId) => { }
-
-// const removeContact = async (contactId) => { }
-
-// const addContact = async (body) => { }
-
-// const updateContact = async (contactId, body) => { }
-
-// module.exports = {
-//   listContacts,
-//   getContactById,
-//   removeContact,
-//   addContact,
-//   updateContact,
-// }
