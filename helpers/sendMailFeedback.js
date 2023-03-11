3549884109f649ad566607c57fc9389e7344ccb5
efirnet <efirnet@gmail.com>
2023-03-11 17:48:40 +0200

    fix

--- helpers/sendMailFeedback.js
+++ helpers/sendMailFeedback.js
@@ -1,33 +1,14 @@
-// require("dotenv").config();
-// const { FROM_EMAIL, PASSWORD } = process.env;
-// const nodemailer = require('nodemailer');
-
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.meta.ua',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     user: FROM_EMAIL,
-//     pass: PASSWORD,
-//   },
-// });
-
-// const sendMailFeedback = async (mailOptions) => {
-//   return await transporter.sendMail(mailOptions);
-// };
-
-// module.exports = sendMailFeedback;
-
-
 require("dotenv").config();
-const { FROM_GMAIL_EMAIL, GMAIL_PASSWORD } = process.env;
+const { FROM_EMAIL, PASSWORD } = process.env;
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  host: 'smtp.meta.ua',
+  port: 465,
+  secure: true,
   auth: {
-    user: FROM_GMAIL_EMAIL,
-    pass: GMAIL_PASSWORD,
+    user: FROM_EMAIL,
+    pass: PASSWORD,
   },
 });
 
@@ -36,3 +17,22 @@ const sendMailFeedback = async (mailOptions) => {
 };
 
 module.exports = sendMailFeedback;
+
+
+// require("dotenv").config();
+// const { FROM_GMAIL_EMAIL, GMAIL_PASSWORD } = process.env;
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//   service: 'Gmail',
+//   auth: {
+//     user: FROM_GMAIL_EMAIL,
+//     pass: GMAIL_PASSWORD,
+//   },
+// });
+
+// const sendMailFeedback = async (mailOptions) => {
+//   return await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendMailFeedback;
