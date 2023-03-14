b8b2f43bd42129f96bf901b833c629e99b927afd
efirnet <efirnet@gmail.com>
2023-03-14 20:48:34 +0200

    del required joi-phone-feedback

--- schemas/feedback.js
+++ schemas/feedback.js
@@ -3,7 +3,7 @@ const Joi = require("joi");
 const feedbackSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string().min(9).max(13).required(),
+  phone: Joi.string().min(9).max(19),
   message: Joi.string().min(10).max(500).trim().required()
 });
 
