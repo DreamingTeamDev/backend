010cc2e41714e89efb7514007261e011e4fde464
efirnet <efirnet@gmail.com>
2023-03-15 20:03:44 +0200

    fix

--- controllers/feedback/sendFeedback.js
+++ controllers/feedback/sendFeedback.js
@@ -1,25 +1,21 @@
-const { FROM_EMAIL,TO_EMAIL } = process.env;
-const { sendMailFeedback } = require('../../helpers');
-require("dotenv").config();
+const { FROM_EMAIL, TO_EMAIL } = process.env
+const { sendMailFeedback } = require('../../helpers')
+require('dotenv').config()
 
 const sendFeedback = async (req, res) => {
   try {
-    const { name, email, phone, message } = req.body;
-    
+    const { name, email, phone, message } = req.body
     const mailOptions = {
       from: FROM_EMAIL,
       to: TO_EMAIL,
       subject: `New message from ${name}`,
-      text: `${message} \n\n Sender name: ${name} \n Sender email: ${email} \n Sender phone: ${phone}`
-    };
-
-    await sendMailFeedback(mailOptions);
-
-    res.send('Message sent');
-
+      text: `${message} \n\n Sender name: ${name} \n Sender email: ${email} \n Sender phone: ${phone}`,
+    }
+    await sendMailFeedback(mailOptions)
+    res.send('Message sent')
   } catch (error) {
-    console.log(error);
-    res.status(500).send('Error sending message');
+    console.log(error)
+    res.status(500).send('Error sending message')
   }
-}; 
-module.exports = sendFeedback;
+}
+module.exports = sendFeedback
