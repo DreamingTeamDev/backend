2ec0af2af6196ac0b2852cd5de03705f6275140a
efirnet <efirnet@gmail.com>
2023-03-15 22:22:52 +0200

    fix from-email

--- controllers/feedback/sendFeedback.js
+++ controllers/feedback/sendFeedback.js
@@ -1,12 +1,12 @@
-const { FROM_EMAIL, TO_EMAIL } = process.env
-const { sendMailFeedback } = require('../../helpers')
+const {/*  FROM_EMAIL,  */TO_EMAIL } = process.env
+const sendMailFeedback = require('../../helpers')
 require('dotenv').config()
 
 const sendFeedback = async (req, res) => {
   try {
     const { name, email, phone, message } = req.body
     const mailOptions = {
-      from: FROM_EMAIL,
+      from: /* FROM_EMAIL */`${email}`,
       to: TO_EMAIL,
       subject: `New message from ${name}`,
       text: `${message} \n\n Sender name: ${name} \n Sender email: ${email} \n Sender phone: ${phone}`,
